2a7d3fd811abce0f6c2fa322a4ee270177b7de6d
Daniil <d.kirykha2010@gmail.com>
2025-08-28 12:00:51 +0300

    Muscles та exercises

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@ import Image from "next/image"
 import Hero from "./components/Hero";
 import Footer from './components/Footer/Footer';
 import QuoteCard from "./components/QuoteCard";
+import Exercises from "./components/Exercises";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Hero />
+        <Hero />\
         <QuoteCard />
-         <Footer />
+        <Exercises />
+        <Footer />
       </main>
-
     </div>
   )
 }
