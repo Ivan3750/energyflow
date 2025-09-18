e652e567180aedf4f230e94384a643af0e56b854
Polina <118378073+Lavrinenko-Polina@users.noreply.github.com>
2025-09-18 16:01:13 +0300

    сторінка контакт

--- app/components/LanguageSwitcher.tsx
+++ app/components/LanguageSwitcher.tsx
@@ -1,3 +1,42 @@
+// "use client";
+
+// import React from "react";
+// import { useTranslate } from "../hooks/useTranslate";
+// import styles from "./LanguageSwitcher.module.css";
+
+// type Lang = "en" | "pl" | "ua";
+
+// const LanguageSwitcher = () => {
+//   const { lang, changeLang } = useTranslate();
+
+//   const languages: { code: Lang; label: string;  }[] = [
+//     { code: "en", label: "EN"},
+//     { code: "pl", label: "PL" },
+//     { code: "ua", label: "UA" },
+//   ];
+
+//   return (
+//     <div className={styles.switcher}>
+//       {languages.map(({ code, label }) => (
+//         <button
+//           key={code}
+//           onClick={() => changeLang(code)}
+//           className={`${styles.btn} ${lang === code ? styles.active : ""}`}
+//           aria-pressed={lang === code}
+//           disabled={lang === code}
+//         >
+          
+//           <span className={styles.label}>{label}</span>
+//         </button>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default LanguageSwitcher;
+
+//////////////////////////////////////////////
+
 "use client";
 
 import React from "react";
@@ -9,28 +48,30 @@ type Lang = "en" | "pl" | "ua";
 const LanguageSwitcher = () => {
   const { lang, changeLang } = useTranslate();
 
-  const languages: { code: Lang; label: string;  }[] = [
-    { code: "en", label: "EN"},
+  const languages: { code: Lang; label: string }[] = [
+    { code: "en", label: "EN" },
     { code: "pl", label: "PL" },
     { code: "ua", label: "UA" },
   ];
 
   return (
     <div className={styles.switcher}>
-      {languages.map(({ code, label }) => (
-        <button
-          key={code}
-          onClick={() => changeLang(code)}
-          className={`${styles.btn} ${lang === code ? styles.active : ""}`}
-          aria-pressed={lang === code}
-          disabled={lang === code}
-        >
-          
-          <span className={styles.label}>{label}</span>
-        </button>
-      ))}
+      <select
+        className={styles.select}
+        value={lang}
+        onChange={(e) => changeLang(e.target.value as Lang)}
+      >
+        {languages.map(({ code, label }) => (
+          <option key={code} value={code}>
+            {label}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
 
 export default LanguageSwitcher;
+
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import InfoBlock from "./components/InfoBlolck"
 import QuoteCard from "./components/QuoteCard";
 import Exercises from "./components/Exercises";
+import Contact from "./components/Contact"
 
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       <div className="container">
         <Hero />
+        <Contact />;
         <QuoteCard />
         <Exercises />
         <InfoBlock />
